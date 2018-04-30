8731ca8ce02de007433465e63368e137dd31c5c4
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-04-30 08:28:48 +0200

    Updating pandoc converting

--- reStructuredText/00_convert_rst_to_Wiki.sh
+++ reStructuredText/00_convert_rst_to_Wiki.sh
@@ -1,1 +1,12 @@
-pandoc --from=rst --to=mediawiki --standalone 1_Moedet_mellem_laege_og_patient.rst --output=1_Moedet_mellem_laege_og_patient.Wiki
+# Manual example
+#pandoc --from=rst --to=mediawiki --table-of-content --standalone 1_Moedet_mellem_laege_og_patient.rst --output=1_Moedet_mellem_laege_og_patient.Wiki
+
+shopt -s nullglob
+#for fullfile in *.rst; do
+RSTFILES=`find . -name "*.rst" -type f`
+for fullfile in $RSTFILES; do
+    filename="$(basename -- "$fullfile")"
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    echo "Converting rst file: " $filename
+done
