fbb36e81350767d26387e3fe670318b98226aa09
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-04-30 21:07:07 +0200

    Adding copy script

---
+++ wikitools/00_cp_Wiki_here.sh
@@ -0,0 +1,15 @@
+# Manual example
+#pandoc --from=rst --to=mediawiki --table-of-content --standalone 1_Moedet_mellem_laege_og_patient.rst --output=1_Moedet_mellem_laege_og_patient.Wiki
+
+shopt -s nullglob
+#for fullfile in *.rst; do
+RSTFILES=`find "../reStructuredText/" -name "*.Wiki" -type f | sort -n`
+for fullfile in $RSTFILES; do
+    filename="$(basename -- "$fullfile")"
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    if [ ! -f ${filename}.Wiki ]; then
+        echo "File not found!" "Copy Wiki file: " ${fullfile}
+        cp ${fullfile} .
+    fi
+done
