5bb648ed5d710097bd3c845079a3d8cebb49d38b
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-04-30 21:27:55 +0200

    Updating info

--- wikitools/test_page.py
+++ wikitools/test_page.py
@@ -4,14 +4,38 @@
 # https://github.com/alexz-enwp/wikitools/wiki/page.Page
 
 from wikitools import wiki, page
+import os.path
 
 site = wiki.Wiki("http://wiki.hoer-laegedansk.dk/api.php")
-#site.login("tlinnet", password="xhwo1453Xl")
-p = page.Page(site, "Forside2")
-#if p.exists:
-# pagetext = p.getWikiText()
+site.login("tlinnet", password="xhwo1453Xl")
 
-# Edit the text
-#p.edit(text=pagetext, summary='Test', minor=True)
-#pagetext = p.getWikiText()
-#print(pagetext)    
+wpages = [
+    "1_Moedet_mellem_laege_og_patient",
+    #"2_Sygehusjournalen",
+    #"3_Indlaeggelsesnotatet"
+]
+
+# loop over pages
+for wpage in wpages:
+    # Check if exists
+    wpage_file = wpage + ".Wiki"
+    if not os.path.exists(wpage_file):
+        continue
+
+    # Get the text of the page
+    F = open(wpage_file, "r") 
+    wpage_text = F.read()
+    F.close()
+    #print(wpage_text)
+
+    # Get the page
+    p = page.Page(site, wpage)
+
+    #if p.exists:
+    # pagetext = p.getWikiText()
+
+    # Edit the text
+    wpage_text = "Testing"
+    p.edit(text=wpage_text, summary='Updating with script', minor=True)
+    pagetext = p.getWikiText()
+    print(pagetext)    
