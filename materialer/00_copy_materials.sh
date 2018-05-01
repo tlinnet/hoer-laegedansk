0628b6adaaf324a4449df026d182ca9fecf5fde1
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-05-01 16:54:03 +0200

    Adding Logo to Wiki

--- materialer/00_copy_materials.sh
+++ materialer/00_copy_materials.sh
@@ -1,9 +1,13 @@
 # Copy favicon
 ##############
-
 ## Copy to wiki
 cp favicon_files/icons/favicon.ico ../wiki/favicon.ico
+
 ## Copy to pelican
 mkdir -p ../static/content/images
 mkdir -p ../static/content/extra
 cp favicon_files/icons/favicon.ico ../static/content/extra/favicon.ico
+
+# Copy Logo
+###########
+cp CD_billede.png ../wiki/resources/assets/CD_billede.png
