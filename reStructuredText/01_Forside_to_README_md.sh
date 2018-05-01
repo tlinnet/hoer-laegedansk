debee0da31fd89515598570289d1c1636033fbd2
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-05-01 12:43:30 +0200

    Update sed script

--- reStructuredText/01_Forside_to_README_md.sh
+++ reStructuredText/01_Forside_to_README_md.sh
@@ -1,3 +1,3 @@
 # Manual example
 pandoc --from=rst --to=markdown --table-of-content --standalone Forside.rst --output=../README.md
-#pandoc --from=rst --to=markdown --standalone Forside.rst --output=../README.md
+gsed -i -n '7,$p' ../README.md
