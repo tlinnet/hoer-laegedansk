3e2f8975af010856f9f1819e8d08d5b30ff4a1da
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-05-01 10:16:49 +0200

    Adding copy script for pelican

---
+++ pelican/00_cp_RST_to_static.sh
@@ -0,0 +1,22 @@
+# Manual example
+#pandoc --from=rst --to=mediawiki --table-of-content --standalone 1_Moedet_mellem_laege_og_patient.rst --output=1_Moedet_mellem_laege_og_patient.Wiki
+
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+shopt -s nullglob
+#for fullfile in *.rst; do
+RSTFILES=`find "../reStructuredText/" -name "*.rst" -type f | sort -n`
+for fullfile in $RSTFILES; do
+    filename="$(basename -- "$fullfile")"
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    #fullpath=$(get_abs_filename "../static/content/${filename}.rst")
+    #if [ ! -f fullpath ]; then
+    if [ ! -f "../static/content/${filename}.rst" ]; then
+        echo "File not found!" "Copy Wiki file: " ${fullfile}
+        cp ${fullfile} ../static/content/
+    fi
+done
