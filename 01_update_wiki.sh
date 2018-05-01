29fd3feb742ffbab16e61a75021950d6ee8e14d5
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-05-01 13:33:12 +0200

    Made script for updating wiki

---
+++ 01_update_wiki.sh
@@ -0,0 +1,22 @@
+
+# Convert .rst files to .Wiki
+cd reStructuredText
+source 00_convert_rst_to_Wiki.sh
+
+# Change dir
+cd ../wikitools
+# Delete .Wiki old files
+rm -f *_*.Wiki
+# Copy over .Wiki files
+source 00_cp_Wiki_here.sh
+
+# Aktivate python
+source activate wikitools
+# Run script for upload
+python test_page.py
+
+# Deactivate
+source deactivate wikitools
+
+# Go back
+cd ..
