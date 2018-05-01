e009cfee85e40f44a8bcda0fdcb44a4de2177b8c
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-05-01 14:30:36 +0200

    Updating pelican

--- materialer/00_copy_materials.sh
+++ materialer/00_copy_materials.sh
@@ -4,6 +4,6 @@
 ## Copy to wiki
 cp favicon_files/icons/favicon.ico ../wiki/favicon.ico
 ## Copy to pelican
-mkdir -p ../static/images
-mkdir -p ../static/extra
-cp favicon_files/icons/favicon.ico ../static/extra/favicon.ico
+mkdir -p ../static/content/images
+mkdir -p ../static/content/extra
+cp favicon_files/icons/favicon.ico ../static/content/extra/favicon.ico
